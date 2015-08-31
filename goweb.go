c66866fccfa5fc45170e64c41620cae47ac65532
Chris Smith <cleesmith2006@gmail.com>
2015-08-31 12:11:38 -0400

    better security; needs more logging

--- goweb.go
+++ goweb.go
@@ -1,38 +1,27 @@
+// this is a more secure httpd, coz it uses:
+// http.FileServer and http.Dir, which have
+// file system checks to avoid break outs
 package main
 
 import (
-	// "errors" // to test panic
-	// "fmt"
+	"fmt"
 	"log"
-	"net"
 	"net/http"
 	"os"
 )
 
-var Info *log.Logger
-
-// var chttp = http.NewServeMux()
+var logit *log.Logger
 
 func main() {
-	Info = log.New(os.Stdout,
+	port := "80"
+	logit = log.New(os.Stdout,
 		"",
 		// log.Ldate|log.Ltime|log.LUTC|log.Lmicroseconds|log.Llongfile)
 		log.Ldate|log.Ltime|log.LUTC|log.Lmicroseconds)
-	// chttp.Handle("/", http.FileServer(http.Dir("./")))
-	http.HandleFunc("/", HomeHandler)
-	err := http.ListenAndServe(":80", nil)
+	fmt.Printf("Serving files in the current directory on port %v\n", port)
+	http.Handle("/", http.FileServer(http.Dir(".")))
+	err := http.ListenAndServe(":"+port, nil)
 	if err != nil {
-		log.Fatal("ListenAndServe: ", err)
+		logit.Fatal("ListenAndServe: ", err)
 	}
-}
-
-func HomeHandler(w http.ResponseWriter, r *http.Request) {
-	host, port, err := net.SplitHostPort(r.RemoteAddr)
-	// err = errors.New("spud") // test panic
-	if err != nil {
-		Info.Println("*** Error: panicking:")
-		panic(err.Error())
-	}
-	Info.Printf("%s \"%s\" for %s : %s\n", r.Method, r.URL.Path, host, port)
-	// chttp.ServeHTTP(w, r)
 }
